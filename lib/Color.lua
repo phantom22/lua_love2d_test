a197e2fd60ff31ab835f742f323a1e4878f8f6d1
phantom22 <maksym0naumenko02@gmail.com>
2025-04-30 22:43:31 +0200

    fix

--- lib/Color.lua
+++
@@ -1,8 +0,0 @@
-Color = {}
-function Color.unpack(c)
-    if #c == 4 then
-        return c[1], c[2], c[3], c[4]
-    else
-        return c[1], c[2], c[3], 1
-    end
-end
--- test.lua
+++
@@ -1,7 +0,0 @@
-require "lib.lib"
-
-local T = Mat2(2,2,2,2)
-local points = Vector({Vec2(1,0), Vec2(0,1), Vec2(-1,0), Vec2(0,-1)})
-
-print(is_class(T))
-print(T * points)
